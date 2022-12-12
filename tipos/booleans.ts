14b9f36579deccb9e38a9f6774a27f0bc769738b
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-12-12 18:19:13 -0300

    numbers

--- tipos/booleans.ts
+++ tipos/booleans.ts
@@ -1,8 +1,14 @@
 ( ()=>{
 
+let isRanma: boolean = true; 
+// let isRanma: boolean = true; //(descomentar y ver las diff)
+console.log(isRanma) 
 
+// si concologueamos el valor de isRanma sin asignarle valor ts nos da error Variable 'isRanma' is used before being assigned.
+// si hacemos esto let isRanma: boolean = true; el erro desaparece
+// teneer en cuenta que esto en js no da error ya el valor de isRanma es undefined, que ts nos dé error o no 
+// depende de las reglas que pongamos en nuestro tsconfig
 
+// 
 
-
-    
 })()// con esta funcion ts entiende que son archivos separados
---
+++ tipos/numbers.js
@@ -0,0 +1,18 @@
+"use strict";
+(() => {
+    let avengers = 30;
+    console.log(avengers);
+    const vilians = 20;
+    if (avengers < vilians) {
+        console.log('Vamos a perder');
+    }
+    else {
+        console.log('Vamos a ganar');
+    }
+    avengers = Number('55A'); // ojo acá esto de NaN y eso es un tipo number 1OjO 
+    console.log(avengers);
+})();
+// en el ejemplo de arriba vemos que ts nos advierte 'avengers' is possibly 'undefined'.
+// pero en js funciona perfecto acá empezamos a ver cómo puede ts ayudar a depurar errores 
+// en el navegador si no acomodamos los datos estp se ejecuta console.log('Vamos a perder')
+//Pero si acomodamos los tipos console.log('Vamos a ganar')
