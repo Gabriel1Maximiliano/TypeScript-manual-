f85194fb6cfa89b6210b5e5291540d38fdd6ac97
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-12-12 19:23:32 -0300

    tuplas

--- tipos/tuplas.js
+++ tipos/tuplas.js
@@ -1,5 +1,5 @@
 "use strict";
-/**
+/** Tupla : Conjunto de elementos de distinto tipo que se guardan de forma consecutiva en memoria.
  * Tuples may be destructured like arrays; the destructuring variables get the types of the corresponding tuple elements:
 
 let tuple: [number, string, boolean] = [7, "hello", true];
